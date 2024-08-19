874c1a9d8b2734b2060a4f97cb4c2afeead42c7f
Brandon Fyall <fyallbrandon@gmail.com>
2024-08-18 18:56:39 -0700

    Built navigation component, implemented React Router, and added initial styling and logo.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+  return <h1>I am the shop page.</h1>;
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />} /> //Here we are rendering specific components based on the URL.
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} /> //Here we are rendering specific components based on the URL.
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
