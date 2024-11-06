370d44740fc508e7f3e150c671bb1cca2315d3ca
Brandon Fyall <fyallbrandon@gmail.com>
2024-11-06 09:28:27 -0800

    Implement sign-up form with user creation, Firebase integration, and form reset

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -15,6 +15,35 @@ const SignUpForm = () => {
 
     console.log(formFields);
 
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields)
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if(password !== confirmPassword) {
+            alert("passwords do not match");
+            return;
+        }
+
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(
+                email, 
+                password
+            );
+
+            await createUserDocumentFromAuth(user, { displayName });
+            resetFormFields();
+        }   catch(error) {
+            if (error.code === 'auth/email-already-in-use') {
+                alert('Cannot create user, email already in use');
+            } else {
+                console.log('user creation encountered an error', error);
+            }        
+        }
+    };
+
     const handleChange = (event) => {
         const {name, value} = event.target;
 
@@ -68,4 +97,3 @@ const SignUpForm = () => {
 };
 
 export default SignUpForm;
-
